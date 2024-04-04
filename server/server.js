2bf96135f971577b4c424aec46d3ec2fea02024d
bmoore639 <142362933+bmoore639@users.noreply.github.com>
2024-04-04 00:14:58 +0000

    4/3/24 5:14PM

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 const port = 3000;
 
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+
+    // res.send('hello world');
 
 });
-
+app.get('/csv-data', (req, res) => {
+    const csvPath = path.join(__dirname, '..', 'data', 'zonal_sea_averages.csv');
+    fs.readFile(csvPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return
+        }
+        res.type('text/plain');
+        res.send(data);
+    });
+})
 
 app.get('/Malcom', (req, res) => {
     res.send("Hi hehe")
