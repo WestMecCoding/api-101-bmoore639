0cfca0d5e53e4d71738ce0c4bbe60e5c9b7d3f4d
bmoore639 <142362933+bmoore639@users.noreply.github.com>
2024-04-05 22:18:40 +0000

    First Graph done

--- server/server.js
+++ server/server.js
@@ -2,17 +2,13 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
-
 const app = express();
 app.use(cors());
 const port = 3000;
 
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
-
-    // res.send('hello world');
-
+    //res.send('hello world');
 });
 app.get('/csv-data', (req, res) => {
     const csvPath = path.join(__dirname, '..', 'data', 'zonal_sea_averages.csv');
@@ -24,14 +20,10 @@ app.get('/csv-data', (req, res) => {
         res.type('text/plain');
         res.send(data);
     });
-})
-
-app.get('/Malcom', (req, res) => {
-    res.send("Hi hehe")
-})
-
-
+});
+app.get('/3pm', (req, res) => {
+    res.send('hello 3pm');
+});
 app.listen(port, () => {
     console.log(`server is listening on port${port}`);
 })
-
